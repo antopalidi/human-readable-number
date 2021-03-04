aad13b83192ef0df4c7fa2ff7c115933457d953f
annatopalidi <topalidisanna@gmail.com>
2021-03-04 23:52:56 +0100

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+    let hundreds = ones[Math.floor(number / 100)] + ' hundred'
+
+    function getTens(number) {
+        if (number < 10) {
+            return ones[number];
+        } else if (number >= 10 && number < 20) {
+            return teens[number - 10];
+        } else {
+            if (number % 10 !== 0) {
+                return tens[Math.floor(number / 10)] + ' ' + ones[number % 10];
+            } else {
+                return tens[Math.floor(number / 10)];
+            }
+        }
+    }
+
+    function getHundreds(number) {
+        if (number > 99) {
+            if (number % 100 !== 0) {
+                return hundreds + ' ' + getTens(number % 100);
+            } else {
+                return hundreds;
+            }
+        } else {
+            return getTens(number);
+        }
+    }
+
+    function getResult(number) {
+        if (number === 0) return 'zero';
+        else return getHundreds(number);
+    }
+
+    return getResult(number);
+
 }
